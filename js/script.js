6dd43227016a3c442b8b51543753b7a56b28b284
Moh Bareche <32602327+MohBareche@users.noreply.github.com>
2024-03-29 06:26:52 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -233,7 +233,7 @@ const lay = L.control.layers.tree(baseTree, overlaysTree, {
   openedSymbol: "&#8863; &#x1f5c1;",
   collapseAll: "Réduire tout",
   expandAll: "Développer tout",
-  collapsed: false,
+  collapsed: true,
 })
 
 lay.addTo(map).collapseTree(true).expandSelected(true)
